8ecbc2e87efe56a0d19323e697134ee523cbf272
Coleyra <masterob1@free.fr>
2018-10-22 00:03:20 +0200

    - Update the init.sql script for the multiple quizz feature

--- init.sql
+++ init.sql
@@ -1,8 +1,14 @@
 CREATE TABLE IF NOT EXISTS `question` (
-`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `type` varchar(20) NOT NULL,
   `label` varchar(200) NOT NULL,
   `answer` varchar(200) NOT NULL,
   `diffusion` int(11) NOT NULL DEFAULT '0',
-  `src` varchar(30) NOT NULL
+  `src` varchar(30) NOT NULL,
+  `id_quizz` int(11) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `quizz` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(500) NOT NULL
 );
